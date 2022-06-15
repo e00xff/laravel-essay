133feaada8d8e7b2a1a54a0a5ab1c8f2b981f593
Levan <e00xff@gmail.com>
2022-06-15 19:42:57 +0400

    layout and static pages created

---
+++ resources/views/toefl.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <header class="py-5 bg-light border-bottom mb-4">
+        <div class="container">
+            <div class="text-center my-5">
+                <h1 class="fw-bolder">About the TOEFL iBT® Test</h1>
+                <p class="lead mb-0">Get the Advantage You Need to Succeed</p>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p>The <span class="text-italic">TOEFL iBT</span><sup class="reg">®</sup> test helps you stand out confidently in English, and it comes with a clear advantage — universities know you are ready to succeed.</p>
+                        <p>That's why it's accepted by more than 11,500 universities and other institutions in over 160 countries, including Australia, Canada, New Zealand, the United States, the U.K., and across Europe and Asia.</p>
+                        <p>The test is conveniently offered via 3 testing options:</p>
+                        <ul>
+                            <li>the traditional TOEFL iBT test in a test center</li>
+                            <li>the TOEFL iBT Home Edition</li>
+                            <li>the TOEFL iBT Paper Edition</li>
+                        </ul>
+                        <p>It's the only test that measures all 4 academic English skills — reading, listening, speaking, and writing — the way they are actually used in a classroom. So your TOEFL® scores provide a true reflection of your abilities and show admissions officers you have what it takes to be great. See the full list of countries where the TOEFL iBT test is available.</p>
+                        <p>Find out more about the test that can help you get where you want to go.</p>
+                        <p>For over 40 years, the TOEFL® research program has pioneered new ways to assess English-language proficiency and made advancements in our understanding of English language learning. Today, the program continues to respond to the needs of test users by researching and developing new assessments for English learners with diverse backgrounds and across age groups.</p>
+                        <p>Learn more about TOEFL research and development in the TOEFL Research Insights Series. Download the TOEFL Research Reference List for information on over 500 publications.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
